57b5537e6dcb1b60b93272300914fba1d6688ace
github-action-benchmark <github@users.noreply.github.com>
2024-02-05 09:33:38 +0000

    add JMH Benchmark (jmh) benchmark result for 97044dd23626d9ac8124e4b3e17da77d8c362378

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1707125618446,
+  "repoUrl": "https://github.com/icodening/benchmark",
+  "entries": {
+    "JMH Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "cleverbn96@gmail.com",
+            "name": "icodening",
+            "username": "icodening"
+          },
+          "committer": {
+            "email": "cleverbn96@gmail.com",
+            "name": "icodening",
+            "username": "icodening"
+          },
+          "distinct": true,
+          "id": "97044dd23626d9ac8124e4b3e17da77d8c362378",
+          "message": "edit rf json test",
+          "timestamp": "2024-02-05T17:31:39+08:00",
+          "tree_id": "c73925b4943b73d352857138b6da6c755b0541b6",
+          "url": "https://github.com/icodening/benchmark/commit/97044dd23626d9ac8124e4b3e17da77d8c362378"
+        },
+        "date": 1707125617614,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.example.benchmark.client.H1.restTemplateBenchmark",
+            "value": 24.844430611061483,
+            "unit": "ops/ms",
+            "extra": "iterations: 3\nforks: 0\nthreads: 32"
+          }
+        ]
+      }
+    ]
+  }
+}
