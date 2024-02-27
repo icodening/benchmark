d71c3f72e9b3c5ee3c6feb46f425867b26cc8aee
icodening <cleverbn96@gmail.com>
2024-02-28 00:04:48 +0800

    Delete client/target directory

--- client/target/generated-sources/annotations/org/example/benchmark/client/generated/H1_jmhType.java
+++
@@ -1,4 +0,0 @@
-package org.example.benchmark.client.generated;
-public class H1_jmhType extends H1_jmhType_B3 {
-}
-
--- client/target/generated-sources/annotations/org/example/benchmark/client/generated/H2C_jmhType.java
+++
@@ -1,4 +0,0 @@
-package org.example.benchmark.client.generated;
-public class H2C_jmhType extends H2C_jmhType_B3 {
-}
-
